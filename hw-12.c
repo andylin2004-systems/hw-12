5d48dae5e497b13ed0af4e3628dd5433c51d47d2
Andy Lin <60021592+andylin2004@users.noreply.github.com>
2021-11-03 21:22:54 -0400

    updated 12

--- hw-12.c
+++ hw-12.c
@@ -2,14 +2,41 @@
 #include <sys/stat.h>
 #include <stdlib.h>
 #include <stdio.h>
+#include <unistd.h>
+#include <errno.h>
+#include <string.h>
 
-int main(){
-    DIR *directory = opendir("./");
+int main(int argc, char *argv[]){
+    char filePath[100];
+    //I have to do this because somehow filePath always gets initalized at the worst locations
+    int i;
+    for (i = 0; i < 100; i++){
+        filePath[i] = '\0';
+    }
+    if (argc > 1)
+    {
+        strncpy(filePath, argv[1], 100);
+    }
+    else
+    {
+        printf("Please provide a directory to look at:\n");
+        read(STDIN_FILENO, filePath, sizeof(filePath));
+        int filePathLen = strlen(filePath);
+        printf("%d", filePathLen);
+        filePath[filePathLen - 1] = '\0';
+    }
+    DIR *directory;
+    directory = opendir(filePath);
+    if (errno != 0)
+    {
+        printf("Error: %s", strerror(errno));
+        return 0;
+    }
     struct dirent *entry;
     struct stat *fileInfo;
     entry = readdir(directory);
     int total = 0;
-    printf("STATS FOR DIR: .\n");
+    printf("STATS FOR DIR: %s\n", filePath);
     while (entry)
     {
         printf("%s ", entry->d_name);
